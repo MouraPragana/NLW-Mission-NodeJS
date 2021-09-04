c05bbf47bc26ac43ed5310558e3efb01bd829bd6
Mateus Pragana <mateuspragana@gmail.com>
2021-09-03 23:28:12 -0300

    Criação do service de criação de tag

---
+++ src/services/CreateTagService.ts
@@ -0,0 +1,35 @@
+import { getCustomRepository } from "typeorm";
+import { TagsRepositories } from "../repositories/TagsRepositories";
+
+interface IRequestTag{
+    name: string;
+}
+
+class CreateTagService {
+    async execute({name}: IRequestTag){
+        
+        const tagRepository = getCustomRepository(TagsRepositories);
+
+        if(!name){
+            throw new Error("Incorrect name!");
+        };
+
+        const tagAlreadyExists = await tagRepository.findOne({
+            name
+        });
+
+        if(tagAlreadyExists){
+            throw new Error("Tag already exists!")
+        };
+
+        const tag = tagRepository.create({
+            name
+        });
+
+        await tagRepository.save(tag);
+
+        return tag;
+    }
+}
+
+export { CreateTagService }
