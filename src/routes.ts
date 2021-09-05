dd979ed63028675bbd80370b26bb7c1ec5e8424f
Mateus Pragana <mateuspragana@gmail.com>
2021-09-05 16:00:25 -0300

    Criando rota get de users e tags

--- src/routes.ts
+++ src/routes.ts
@@ -7,6 +7,8 @@ import { CreateComplimentController } from './controllers/CreateComplimentContro
 import { ensureAuthenticated } from './middlewares/ensureAuthenticated';
 import { ListUserSendComplimentsController } from './controllers/ListUserSendComplimentsController';
 import { ListUserReceiveComplimentsController } from './controllers/ListUserReceiveComplimentsController';
+import { ListTagsController } from './controllers/ListTagsController';
+import { ListUsersController } from './controllers/ListUsersController';
 
 const router = Router();
 
@@ -18,6 +20,8 @@ const listUserSendComplimentsController =
   new ListUserSendComplimentsController();
 const listUserReceiveComplimentsController =
   new ListUserReceiveComplimentsController();
+const listTagsController = new ListTagsController();
+const listUsersController = new ListUsersController();
 
 router.post('/users', createUserController.handle);
 router.post(
@@ -26,13 +30,14 @@ router.post(
   ensureAdmin,
   createTagController.handle
 );
+router.get('/users', ensureAuthenticated, listUsersController.handle);
+router.get('/tags', ensureAuthenticated, listTagsController.handle);
 router.post('/login', authenticateUserController.handle);
 router.post(
   '/compliments',
   ensureAuthenticated,
   createComplimentController.handle
 );
-
 router.get(
   '/users/compliments/send',
   ensureAuthenticated,
