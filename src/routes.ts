f5ee581ea43abb280ed21e8a4aa285f9590fe9c4
Mateus Pragana <mateuspragana@gmail.com>
2021-09-05 15:13:43 -0300

    Adicionado rotas de listagem de compliments

--- src/routes.ts
+++ src/routes.ts
@@ -5,13 +5,19 @@ import { ensureAdmin } from './middlewares/ensureAdmin';
 import { AuthenticateUserController } from './controllers/AuthenticateUserController';
 import { CreateComplimentController } from './controllers/CreateComplimentController';
 import { ensureAuthenticated } from './middlewares/ensureAuthenticated';
+import { ListUserSendComplimentsController } from './controllers/ListUserSendComplimentsController';
+import { ListUserReceiveComplimentsController } from './controllers/ListUserReceiveComplimentsController';
 
 const router = Router();
 
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
 const authenticateUserController = new AuthenticateUserController();
-const createComplimentcontroller = new CreateComplimentController();
+const createComplimentController = new CreateComplimentController();
+const listUserSendComplimentsController =
+  new ListUserSendComplimentsController();
+const listUserReceiveComplimentsController =
+  new ListUserReceiveComplimentsController();
 
 router.post('/users', createUserController.handle);
 router.post(
@@ -24,7 +30,18 @@ router.post('/login', authenticateUserController.handle);
 router.post(
   '/compliments',
   ensureAuthenticated,
-  createComplimentcontroller.handle
+  createComplimentController.handle
+);
+
+router.get(
+  '/users/compliments/send',
+  ensureAuthenticated,
+  listUserSendComplimentsController.handle
+);
+router.get(
+  '/users/compliments/receive',
+  ensureAuthenticated,
+  listUserReceiveComplimentsController.handle
 );
 
 export { router };
